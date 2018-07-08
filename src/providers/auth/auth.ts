06415638d0f10bf8cb47b7016dbab3e05100b187
Mateus Manica <mateus.manica@sap.com>
2018-07-08 10:35:38 -0300

    auth.ts now just works with authentication; publish offer method was removed from auth.ts

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -41,24 +41,4 @@ export class AuthProvider {
     });
   }
 
-  async publishGameOffer(gameOffer: any): Promise<any> {
-    try{
-      const offerUuid = uuid();
-      const publishableGameOffer = gameOffer;
-      publishableGameOffer.status = 'open';
-      publishableGameOffer.userID = firebase.auth().currentUser.uid;
-      publishableGameOffer.offerID = offerUuid;
-      return await firebase.database()
-        .ref(`/offeredGames/${offerUuid}`).set(publishableGameOffer)
-        .then( done => {
-          console.log('publishGameOffer, Then(done): ');
-          console.log(done);
-        });
-      
-    }catch(error) {
-      console.log('lancei erro dentro da funcao');
-      throw error;
-    }
-  }
-
 }
